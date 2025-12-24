52da43d82f125bc99b785a4d0626b3214f5c222d
Laura Gularte <laurabgularte@gmail.com>
2025-12-24 07:58:11 -0300

    add basic commands file

---
+++ basics/commands.py
@@ -0,0 +1,49 @@
+# SOME BASIC COMMANDS IN PYTHON
+
+print("Hello, World!")
+
+
+user_input = input ("enter your name:")
+print (f"Hello, {user_input}!")
+
+x = 10
+print ( type (x)) # Output: <class ‘int’ >
+
+my_string = "Python"
+
+print (len (my_string)) # Output: 6
+
+for i in range (3):
+
+    print (i) # Output: 0, 1, 2
+
+numbers = [1, 2, 3, 4]
+
+print ( sum (numbers )) #Output: 10
+
+
+
+#SOME TERMINAL COMMANDS FOR PYTHON 
+
+
+$python
+
+>>> print (“Welcome to python shell!”)
+
+exit()
+
+help(print)
+
+$ pip install mumpy
+
+# SOME FILE AND DIRECTORY COMMANDS 
+
+file = open ( ‘example.txt’ , ‘w’)
+file. write (“Hello, World!”)
+file. close ()
+
+file = open (‘example. txt' , ‘r’)
+tent = file.read ()
+print (content ) # Output: Hello, World!
+
+
